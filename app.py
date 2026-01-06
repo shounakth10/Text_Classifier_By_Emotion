180ed3a185b9612ce794e2d2c3dffc4d0dd8722a
shounakth10 <shounakth10@gmail.com>
2026-01-06 13:09:56 +0000

    Initial Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import joblib
-from track_utils import create_page_visited_table, add_page_visited_details, view_all_page_visited_details, add_prediction_details, view_all_prediction_details, create_emotionclf_table, IST
+from track_utils import add_prediction_details, view_all_prediction_details, create_emotionclf_table, IST
 
 # Load Model
 pipe_lr = joblib.load(open("./models/emotion_classifier_pipe_lr.pkl", "rb"))
